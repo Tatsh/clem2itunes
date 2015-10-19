f0bd40dcbc69d720411b4b2e69cefb6b0e6f4986
Andrew Udvare <audvare@gmail.com>
2015-10-18 20:59:19 -0700

    Less hard-coding

--- run.sh
+++ run.sh
@@ -2,10 +2,12 @@
 set -o errexit
 
 FROM_USER=tatsh
-FROM_HOST=tat.sh
+FROM_HOST=limelight
 LOCAL_DIR="${HOME}/Music/import"
-REMOTE_DIR="/mnt/tatsh/temp/import"
+REMOTE_DIR="/home/tatsh/temp/import"
+PYTHON='/home/tatsh/.virtualenvs/clem2itunes/bin/python'
+CREATE_LIB='/home/tatsh/dev/clem2itunes/clem2itunes-create-lib'
 
-ssh "${FROM_USER}@${FROM_HOST}" "/home/tatsh/.virtualenvs/clem2itunes/bin/python /home/tatsh/dev/clem2itunes/clem2itunes-create-lib -m 32 --split-dir /mnt/tatsh4t-2/splitcue-cache/ ${REMOTE_DIR}"
+ssh "${FROM_USER}@${FROM_HOST}" "${PYTHON} ${CREATE_LIB} -m 32 --split-dir /home/tatsh/.splitcue-cache/ ${REMOTE_DIR}"
 rsync --force --delete-before -rtdLqc "${FROM_USER}@${FROM_HOST}:${REMOTE_DIR}/" "$LOCAL_DIR"
 coffee -p update-itunes.coffee | osascript -l JavaScript
