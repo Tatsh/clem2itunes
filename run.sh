3e2e189b754319e7858361666c87027928d9a2cf
Andrew Udvare <audvare@gmail.com>
2016-01-23 12:51:49 -0800

    Make a lot of things optional with defaults; shellcheck

--- run.sh
+++ run.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 set -o errexit
 
-FROM_USER=tatsh
-FROM_HOST=limelight
-LOCAL_DIR="${HOME}/Music/import"
-REMOTE_DIR="/home/tatsh/temp/import"
-PYTHON='/home/tatsh/.virtualenvs/clem2itunes/bin/python'
-CREATE_LIB='/home/tatsh/dev/clem2itunes/clem2itunes-create-lib'
+FROM_USER="${FROM_USER:-${USER}}"
+FROM_HOST="${FROM_HOST:-limelight}"
+LOCAL_DIR="${LOCAL_DIR:-${HOME}/Music/import}"
+REMOTE_DIR="${REMOTE_DIR:-/home/${USER}/temp/import}"
+REMOTE_PYTHON="${REMOTE_PYTHON:-/home/${USER}/.virtualenvs/clem2itunes/bin/python}"
+REMOTE_CREATE_LIB="${REMOTE_CREATE_LIB:-/home/${USER}/dev/clem2itunes/clem2itunes-create-lib}"
+REMOTE_SPLITCUE_CACHE_DIR="${REMOTE_SPLITCUE_CACHE_DIR:-/home/${USER}/.splitcue-cache/}"
+SIZE_LIMIT_IN_GB="${SIZE_LIMIT_IN_GB:-32}"
 
-ssh "${FROM_USER}@${FROM_HOST}" "${PYTHON} ${CREATE_LIB} -m 32 --split-dir /home/tatsh/.splitcue-cache/ ${REMOTE_DIR}"
+# shellcheck disable=SC2029
+ssh "${FROM_USER}@${FROM_HOST}" \
+    "${REMOTE_PYTHON} \
+     ${REMOTE_CREATE_LIB} \
+        -m ${SIZE_LIMIT_IN_GB} \
+        --split-dir \
+        ${REMOTE_SPLITCUE_CACHE_DIR} \
+        ${REMOTE_DIR}"
 rsync --force --delete-before -rtdLqc "${FROM_USER}@${FROM_HOST}:${REMOTE_DIR}/" "$LOCAL_DIR"
 coffee -p update-itunes.coffee | osascript -l JavaScript
